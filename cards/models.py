9b04bdf3ddf3332e4413c89adc88c611bebae3f1
Michael Lewis <michaeladamlewis@gmail.com>
2015-05-05 12:52:03 -0700

    added some bootstrap and extra fields

--- cards/models.py
+++ cards/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
 class Card(models.Model):
-	name = models.CharField(max_length=255)
-	card_number = models.CharField(max_length=12)
+	name = models.CharField(max_length=255, null=True, blank=True)
+	card_number = models.CharField(max_length=12, null=True, blank=True)
 	description = models.CharField(max_length=255, null=True, blank=True)
 	def __unicode__(self):
 		return self.name
 
 class Deck(models.Model):
-	name = models.CharField(max_length=255)
+	name = models.CharField(max_length=255, null=True, blank=True)
 	cards = models.ManyToManyField(Card)
 	description = models.CharField(max_length=255, null=True, blank=True)
+	author = models.CharField(max_length=255, null=True, blank=True)
 	def __unicode__(self):
 		return self.name
 
